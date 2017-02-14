0cd281defb17cbbfc092e29822dee91196a2f2de
p1le <anon@tymoon.eu>
2017-02-13 19:29:15 -0500

    Practising making some dumb math functions

--- morelisp.lisp
+++ morelisp.lisp
@@ -1,2 +1,15 @@
-(defun morelisp ()
-  (format t "Hello, there!"))
+(defun half (n)
+  (format t "Half of ~d is: " n)
+  (/ n 2.0))
+
+(defun cube (n)
+  (format t "The cube of ~d is: " n)
+  (* n n n))
+
+(defun onemorep (n)
+  (format t "One more of ~d is: " n)
+  (incf n))
+
+(defun pythag (x y)
+  (sqrt (+ (* x x) (* y y))))
+
