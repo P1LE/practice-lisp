ea38ab06a233a9c6537a29621e25f2d870547f58
p1le <anon@tymoon.eu>
2017-03-02 12:59:04 -0500

    more boring exercises

---
+++ gentle/ch6-data-structures/ch6p1.lisp
@@ -0,0 +1,18 @@
+#|
+ Author: p1le <anon@tymoon.eu>
+ Chapter 6 - List Data Structures
+|#
+
+;;Exercise 6.5
+(defvar line '(roses are red))
+
+
+;;(reverse line) => (RED ARE ROSES)
+;;(first (last line)) => RED
+;;(nth 1 line) => ARE
+;;(reverse (reverse line)) => (ROSE ARE RED)
+;;(append line (list (first line))) => (ROSES ARE RED ROSES)
+;;(append (last line) line) => (RED ROSES ARE RED)
+;;(list (first line) (last line)) => (ROSES (RED))
+;;(cons (last line) line) => ((RED) ROSES ARE RED)
+;;(remove 'are line) => (ROSES RED)
