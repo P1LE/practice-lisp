bc46b5fa4f3afac681ea6970410b69c4c76e908d
Jim <anon@tymoon.eu>
2018-09-11 00:25:41 -0400

    Switch some lines around.

--- adventure.lisp
+++ adventure.lisp
@@ -9,8 +9,7 @@
                         (attic (you are in the attic.
                                 there is a giant welding torch in the corner.))))
 
-(defun describe-location (location nodes)
-  (cadr (assoc location nodes)))
+
 
 (defparameter *edges* '((living-room (garden west door)
                          (attic upstairs ladder))
@@ -25,6 +24,11 @@
                                    (frog garden)))
 
 (defparameter *location* 'living-room)
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
 
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
@@ -65,3 +69,41 @@
 
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+(defun game-repl ()
+  (let ((cmd (game-read)))
+    (unless (eq (car cmd) 'quit)
+      (game-print (game-eval cmd))
+      (game-repl))))
+
+(defun game-read ()
+  (let ((cmd (read-from-string
+              (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x)
+             (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+      (eval sexp)
+      '(i do not know that command.)))
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst)))
+      (cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eq item #\") (tweak-text rest caps (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+            ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+            (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() "
+                                                  (prin1-to-string lst))
+                                     'list)
+                             t
+                             nil)
+                 'string))
+  (fresh-line))
